c264be055837f014b47b34532e5bb7a3c9c9a9ed
Marvin <marvin.mardiat@brainart.ee>
2020-11-30 22:57:03 +0200

    Initial commit

---
+++ classes/Utils.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Classes;
+
+use RuntimeException;
+
+class Utils
+{
+    public static function getInput($argc, $argv): array
+    {
+        if ($argc && $argc > 1) {
+            if (file_exists(realpath($argv[1]))) {
+                $input = file(realpath($argv[1]));
+                if (!$input) {
+                    throw new RuntimeException('Failure upon file read!');
+                }
+            } else {
+                throw new RuntimeException('Invalid input file: ' . $argv[1]);
+            }
+        } else {
+            throw new RuntimeException('No input file!');
+        }
+
+        return $input;
+    }
+}
---
+++ day01/main.php
@@ -0,0 +1,10 @@
+<?php
+require "vendor/autoload.php";
+
+use Classes\Utils;
+
+$input = Utils::getInput($argc, $argv);
+
+// Day 1 goes here
+
+echo PHP_EOL;
