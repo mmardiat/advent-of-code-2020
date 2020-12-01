898a9feee717c5c002909daa64285ec0e684a0b5
Marvin <marvin.mardiat@brainart.ee>
2020-12-01 07:45:46 +0200

    Day 01 finished

--- day01/main.php
+++ day01/main.php
@@ -3,8 +3,61 @@ require "vendor/autoload.php";
 
 use Classes\Utils;
 
-$input = Utils::getInput($argc, $argv);
+$inputs = Utils::getInput($argc, $argv);
 
-// Day 1 goes here
+// First part
+$inputKey = 0;
+foreach ($inputs as $key => $value) {
+    if (cycleValues($inputs, $inputKey)) {
+        break;
+    }
 
-echo PHP_EOL;
+    ++$inputKey;
+}
+
+echo '----'  . PHP_EOL;
+
+// Second part
+$k = 0;
+foreach ($inputs as $key => $value) {
+    if (cycleValues($inputs, $k, true)) {
+        break;
+    }
+
+    ++$k;
+}
+
+function cycleValues($inputs, $inputKey, $thirdOperator = false)
+{
+    foreach ($inputs as $key => $value) {
+        if ($key === $inputKey) {
+            continue;
+        }
+
+        $value = (int) trim($value);
+        $value2 = (int) trim($inputs[$inputKey]);
+
+        if ($thirdOperator) {
+            foreach ($inputs as $k => $v) {
+                if ($k === $inputKey || $k === $key) {
+                    continue;
+                }
+                $value3 = (int) trim($v);
+
+                if ($value + $value2 + $value3 === 2020) {
+                    echo sprintf('Values are %d and %d and %d', $value, $value2, $value3) . PHP_EOL;
+                    echo sprintf('Their multiplication is %d', $value * $value2 * $value3)  . PHP_EOL;
+
+                    return true;
+                }
+            }
+        } else if ($value + $value2 === 2020) {
+            echo sprintf('Values are %d and %d', $value, $value2) . PHP_EOL;
+            echo sprintf('Their multiplication is %d', $value * $value2) . PHP_EOL;
+
+            return true;
+        }
+    }
+
+    return false;
+}
